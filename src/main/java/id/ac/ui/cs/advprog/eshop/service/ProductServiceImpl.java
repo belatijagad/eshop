72160b211672c3d1281b1c11b571b06fd145d3de
Belati <belatijagadbintangsyuhada@gmail.com>
2024-02-07 14:22:09 +0700

    added edit for product service

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
@@ -28,4 +28,9 @@ public class ProductServiceImpl implements ProductService {
         productIterator.forEachRemaining(allProduct::add);
         return allProduct;
     }
+
+    @Override 
+    public Product getById(String productId) {
+        return productRepository.getById(productId);
+    }
 }
