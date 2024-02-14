a0871d1873b5d780bcb9a616ab8e12bfbd6f87df
Belati <belatijagadbintangsyuhada@gmail.com>
2024-02-14 19:05:32 +0700

    coba lagi

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 sonar {
     properties {
         property("sonar.projectKey", "belatijagad_eshop")
-        property("sonar.organization", "Belati")
+        property("sonar.organization", "belati")
         property("sonar.host.url", "https://sonarcloud.io")
     }
 }
