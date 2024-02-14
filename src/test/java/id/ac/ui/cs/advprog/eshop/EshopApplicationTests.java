8f584f2571a8c7f3f53d22890008d2a7f9c9507c
Belati <belatijagadbintangsyuhada@gmail.com>
2024-02-14 15:55:14 +0700

    product repository and main app test

--- src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
@@ -1,5 +1,7 @@
 package id.ac.ui.cs.advprog.eshop;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,11 @@ class EshopApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(true, "The context loads successfully");
 	}
 
+	@Test
+    void testMain() {
+        EshopApplication.main(new String[] {});
+    }
 }
--- src/test/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepositoryTest.java
@@ -8,10 +8,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.mock.mockito.MockBean;
+// import org.springframework.boot.test.mock.mockito.MockBean;
 
 
 import java.util.Iterator;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -27,9 +28,40 @@ public class ProductRepositoryTest {
     }
 
     @Test
-    void testCreateAndFind() {
+    void testGetById() {
+        String productId = UUID.randomUUID().toString();
+
         Product product = new Product();
-        product.setProductId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        product.setProductId(productId);
+        product.setProductName("Sampo Cap Bambang");
+        product.setProductQuantity(69);
+        productRepository.create(product);
+
+        Product returnedProduct = productRepository.getById(productId);
+        assertNotNull(returnedProduct);
+    }
+
+    @Test
+    void testGetByIdNonexisting() {
+        String productId1 = UUID.randomUUID().toString();
+        String productId2 = UUID.randomUUID().toString();
+
+        Product product = new Product();
+        product.setProductId(productId1);
+        product.setProductName("Sampo Cap Bambang");
+        product.setProductQuantity(69);
+        productRepository.create(product);
+
+        Product returnedProduct = productRepository.getById(productId2);
+        assertNull(returnedProduct);
+    }
+
+    @Test
+    void testCreateAndFind() {
+        String productId = UUID.randomUUID().toString();
+
+        Product product = new Product();
+        product.setProductId(productId);
         product.setProductName("Sampo Cap Bambang");
         product.setProductQuantity(69);
         productRepository.create(product);
@@ -50,14 +82,16 @@ public class ProductRepositoryTest {
 
     @Test
     void testFindAllIfMoreThanOneProduct() {
+        String productId1 = UUID.randomUUID().toString();
         Product product1 = new Product();
-        product1.setProductId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        product1.setProductId(productId1);
         product1.setProductName("Sampo Cap Bambang");
         product1.setProductQuantity(69);
         productRepository.create(product1);
 
+        String productId2 = UUID.randomUUID().toString();
         Product product2 = new Product();
-        product2.setProductId("7f610505-e444-40ea-ab57-a86b56a2b9fc");
+        product2.setProductId(productId2);
         product2.setProductName("Sampo Cap Yu");
         product2.setProductQuantity(69);
         productRepository.create(product2);
@@ -72,14 +106,16 @@ public class ProductRepositoryTest {
 
     @Test 
     void testCreateUpdateThenFind() {
+        String productId = UUID.randomUUID().toString();
+
         Product product = new Product();
-        product.setProductId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        product.setProductId(productId);
         product.setProductName("Sampo Cap Bambang");
         product.setProductQuantity(69);
         productRepository.create(product);
 
         Product updatedProduct = new Product();
-        updatedProduct.setProductId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        updatedProduct.setProductId(productId);
         updatedProduct.setProductName("Sampo Cap Budi");
         updatedProduct.setProductQuantity(420);
         productRepository.update(updatedProduct);
@@ -88,18 +124,74 @@ public class ProductRepositoryTest {
         assertEquals(product.getProductQuantity(), updatedProduct.getProductQuantity());
     }
 
+    @Test
+    void testUpdateQuantityNotNegative() {
+        String productId = UUID.randomUUID().toString();
+
+        Product product = new Product();
+        product.setProductId(productId);
+        product.setProductName("Sampo Cap Bambang");
+        product.setProductQuantity(69);
+        productRepository.create(product);
+
+        Product updatedProduct = new Product();
+        updatedProduct.setProductId(productId);
+        updatedProduct.setProductName("Sampo Cap Budi");
+        updatedProduct.setProductQuantity(-3);
+        productRepository.update(updatedProduct);
+
+        assertEquals(product.getProductQuantity(), 0);
+    }
+
+    @Test
+    void testUpdateNonexistingProduct() {
+        String productId1 = UUID.randomUUID().toString();
+        String productId2 = UUID.randomUUID().toString();
+
+        Product product = new Product();
+        product.setProductId(productId1);
+        product.setProductName("Sampo Cap Bambang");
+        product.setProductQuantity(69);
+        productRepository.create(product);
+
+        Product updatedNonexisting = new Product();
+        updatedNonexisting.setProductId(productId2);
+        updatedNonexisting.setProductName("Sampo Cap Budi");
+        updatedNonexisting.setProductQuantity(420);
+        Product returnedProduct = productRepository.update(updatedNonexisting);
+
+        assertNull(returnedProduct);
+    }
+
     @Test 
     void testCreateDeleteThenFind() {
+        String productId = UUID.randomUUID().toString();
+
         Product product = new Product();
-        product.setProductId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        product.setProductId(productId);
         product.setProductName("Sampo Cap Bambang");
         product.setProductQuantity(69);
         productRepository.create(product);
 
         Iterator<Product> productIterator = productRepository.findAll();
         assertTrue(productIterator.hasNext());
-        productRepository.deleteById("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        productRepository.deleteById(productId);
         productIterator = productRepository.findAll();
         assertFalse(productIterator.hasNext());
+    }
+
+    @Test
+    void testDeleteNonexisting() {
+        String productId1 = UUID.randomUUID().toString();
+        String productId2 = UUID.randomUUID().toString();
+
+        Product product = new Product();
+        product.setProductId(productId1);
+        product.setProductName("Sampo Cap Bambang");
+        product.setProductQuantity(69);
+        productRepository.create(product);
+
+        Boolean isProductDeleted = productRepository.deleteById(productId2);
+        assertFalse(isProductDeleted);
     }
 }
