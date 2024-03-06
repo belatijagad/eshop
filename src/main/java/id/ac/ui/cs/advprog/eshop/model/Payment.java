02a4bca7217a337c8b62b923e67e21308e118a31
Belati <belatijagadbintangsyuhada@gmail.com>
2024-03-06 19:47:01 +0700

    Revert "[REFACTOR] Apply PaymentStatus enum to Payment model and test."
    This reverts commit 51272f579e82afe68a7d905d9ecea6fb579b14c5.

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -1,6 +1,5 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
-import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
 import lombok.Getter;
 
 import java.util.Map;
@@ -16,7 +15,7 @@ public class Payment {
         this.id = id;
         this.method = method;
         this.paymentData = paymentData;
-        this.status = PaymentStatus.REJECTED.getValue();
+        this.status = "REJECTED";
     }
 
     public Payment (String id, String method, Map <String, String> paymentData, String status) {
@@ -25,7 +24,7 @@ public class Payment {
     }
 
     public void setStatus(String status) {
-        if (PaymentStatus.contains(status)) {
+        if (status.equals("SUCCESS") || status.equals("REJECTED")) {
             this.status = status;
         } else {
             throw new IllegalArgumentException();
--- src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
@@ -1,6 +1,5 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
-import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -19,36 +18,32 @@ class PaymentTest {
 
     @Test
     void testCreatePaymentDefaultStatus() {
-        Payment payment = new Payment("13652556-012a-4c07-b546-54eb1396d79b",
-                "VOUCHER", this.paymentData);
+        Payment payment = new Payment("13652556-012a-4c07-b546-54eb1396d79b", "VOUCHER", this.paymentData);
         assertEquals("13652556-012a-4c07-b546-54eb1396d79b", payment.getId());
         assertEquals("VOUCHER", payment.getMethod());
-        assertEquals(PaymentStatus.REJECTED.getValue(), payment.getStatus());
+        assertEquals("REJECTED", payment.getStatus());
         assertSame(this.paymentData, payment.getPaymentData());
     }
 
     @Test
     void testCreatePaymentSuccessStatus() {
-        Payment payment = new Payment("13652556-012a-4c07-b546-54eb1396d79b",
-                "VOUCHER", this.paymentData, PaymentStatus.SUCCESS.getValue());
+        Payment payment = new Payment("13652556-012a-4c07-b546-54eb1396d79b", "VOUCHER", this.paymentData, "SUCCESS");
         assertEquals("13652556-012a-4c07-b546-54eb1396d79b", payment.getId());
         assertEquals("VOUCHER", payment.getMethod());
-        assertEquals(PaymentStatus.SUCCESS.getValue(), payment.getStatus());
+        assertEquals("SUCCESS", payment.getStatus());
         assertSame(this.paymentData, payment.getPaymentData());
     }
 
     @Test
     void testCreatePaymentInvalidStatus() {
         assertThrows (IllegalArgumentException.class, () -> {
-            Payment payment = new Payment("13652556-012a-4c07-b546-54eb1396d79b",
-                    "VOUCHER", this.paymentData, "INVALID");
+            Payment payment = new Payment("13652556-012a-4c07-b546-54eb1396d79b", "VOUCHER", this.paymentData, "INVALID");
         });
     }
 
     @Test
     void testSetPaymentDataEmpty() {
-        Payment payment = new Payment("13652556-012a-4c07-b546-54eb1396d79b",
-                "VOUCHER", this.paymentData);
+        Payment payment = new Payment("13652556-012a-4c07-b546-54eb1396d79b", "VOUCHER", this.paymentData);
         this.paymentData.clear();
         assertThrows (IllegalArgumentException.class, () -> {
             payment.setPaymentData(this.paymentData);
@@ -57,8 +52,7 @@ class PaymentTest {
 
     @Test
     void testSetPaymentDataSuccess() {
-        Payment payment = new Payment("13652556-012a-4c07-b546-54eb1396d79b",
-                "VOUCHER", this.paymentData);
+        Payment payment = new Payment("13652556-012a-4c07-b546-54eb1396d79b", "VOUCHER", this.paymentData);
         this.paymentData.put("voucherCode", "ESHOP1234ABC5678");
         payment.setPaymentData(this.paymentData);
         assertSame(this.paymentData, payment.getPaymentData());
